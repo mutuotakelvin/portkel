f9b0098657e5e3392c48cfdf1b420578f027b393
mutuotakelvin <j17-3030-2019@student.mksu.ac.ke>
2021-11-18 03:07:27 +0300

    modification

--- src/data.js
+++ src/data.js
@@ -44,9 +44,9 @@ export const testimonials = [
   {
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Evan Nguku",
-    company: "Dooble.io",
+    image: "./tom.jpg",
+    name: "Thomas Mwaniki",
+    company: "Mwanikis.io",
   },
 ];
 
